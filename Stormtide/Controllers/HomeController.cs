a50aeec347f119c0f84ae0f1fac8f20a41143247
Ezra <ezramcnichols@gmail.com>
2020-10-31 03:01:06 -0500

    catchall!

--- Stormtide/Controllers/HomeController.cs
+++ Stormtide/Controllers/HomeController.cs
@@ -10,12 +10,14 @@ namespace Stormtide.Controllers
         }
 
         [Route("SpaceAlertResolver")]
+        [Route("SpaceAlertResolver/{*catchall}")]
         public ActionResult SpaceAlertResolver()
         {
             return Redirect("https://space-alert-resolver.stormtide.net");
         }
 
         [Route("Avery16282Generators")]
+        [Route("Avery16282Generators/{*catchall}")]
         public ActionResult Avery16282Generators()
         {
             return Redirect("https://avery-16282-generators.stormtide.net");
